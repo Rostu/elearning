378fd84830e2ad0f34c5143304c7b566c2365269
Robert S <robert.studtrucker@gmail.com>
2014-07-05 10:36:21 +0200

    Erweiterung der Seite Zukunft

--- public/javascripts/Zukunft.js
+++ public/javascripts/Zukunft.js
@@ -1,29 +1,38 @@
+
 $( init );
 function init() {
     var id=0;
-
-
-    $("div[id=inline]").each(function() {
-        //console.log(this);
-        $(this).data('id',id).draggable({
+    $("div[class=inline]").each(function() {
+        $(this).attr('id',id).draggable({
             containment: '#page',
             revert: true,
             helper: myHelper});
         id++;
-        console.log($(this).data('id'));
-        console.log($(this).data('technik'));
     });
-
-$('#Antwortbox').droppable( {
-    drop: handleDropEvent
-    } );
-function handleDropEvent( event, ui ) {
-    $("#Antwortbox").append('<div id="token">Klima</div>')
-    $('#draggie').remove()
-    ui.draggable.css('color','red');
-    ui.draggable.draggable('disable');
-    }
-function myHelper( event ) {
-    return '<div id="draggie">Klima</div>';
-    }
-}
+    $('#Antwortbox').droppable( {
+        drop: handleDropEvent
+    });
+    function handleDropEvent( event, ui ) {
+        var div = jQuery('<div/>', {
+            class: 'token',
+            id: ui.draggable.attr('id'),
+            text: ui.draggable.context.textContent
+        });
+        $("#Antwortbox").append(div);
+        div.click(clickhandler);
+        ui.draggable.css('color','red');
+        ui.draggable.draggable('disable');
+    };
+    function myHelper( event ) {
+        return jQuery('<div/>', {
+            class: 'draggie',
+            id: $(this).attr('id'),
+            text: $(this).context.innerHTML
+        });
+    };
+    function clickhandler() {
+        $("div[class=inline.ui-draggable][id="+this.id+"]").draggable("enable");
+        $("div[class=inline.ui-draggable][id="+this.id+"]").css('color','black');
+        $(this).remove();
+    };
+};
