59cc82da2308fd4d4e331e17ef00effaf7079d42
Robert S <robert.studtrucker@gmail.com>
2014-07-08 13:44:55 +0200

    kleine Änderungen an Zukunft.js

--- public/javascripts/Zukunft.js
+++ public/javascripts/Zukunft.js
@@ -2,17 +2,15 @@
 $( init );
 function init() {
     var id=0;
-    var testarray = [true,true,false,true];
-    testarray.forEach(function(entry) {
-        if(entry){
-        console.log(entry);}
-    });
+    var id_speicher = [];
+    var testarray = [[false],[true],[false],[true],[true],[false],[false],[false],[true],[false],[false],[false],[false],[false],[true],[false],[false],[false],[false],[false],[false],[false],[false],[false],[false],[false],[false],[false],[true],[true],[true],[false],[false],[false],[false],[false],[false],[false],[true],[false],[true],[true],[false],[false],[false],[false],[false],[true],[false],[true],[false],[false],[true],[false],[true],[true],[true],[true],[true],[true],[true],[true],[true],[false],[true],[true],[false],[false],[false],[true],[false],[true],[true],[true],[true],[true],[true],[false],[true],[true],[false],[true],[true],[false],[false],[false],[true],[true],[false],[false],[false],[false],[true],[false],[false],[false],[true],[true],[true],[true],[false],[false],[true],[false],[false],[false],[true],[true],[false],[true],[false],[false]];
+    var i = 0;
+
     $("div[class=inline]").each(function() {
-        $(this).attr('id',id).draggable({
+        $(this).draggable({
             containment: '#page',
             revert: true,
             helper: myHelper});
-        id++;
     });
     $('#Antwortbox').droppable( {
         drop: handleDropEvent
@@ -21,13 +19,23 @@ function init() {
         var div = jQuery('<div/>', {
             class: 'token',
             id: ui.draggable.attr('id'),
-            text: ui.draggable.context.textContent
-        });
+            html: "<p>"+ui.draggable.context.textContent+"</p><div id='"+ui.draggable.attr('id') +"' class='hide'><p>Das ist ein kleiner Testtext. An dieser Stelle soll dann eine kurze erläuterung zu dem Wort erscheinen.</p><a target='_blank' href='http://www.duden.de/suchen/dudenonline/"+ui.draggable.context.textContent+"'>INFO-Link</a><p><button id='"+ui.draggable.attr('id')+"'>löschen</button></p></div>"
+            });
+        div.click(enlarge);
+        $(div).data('korrekt',testarray[i][0]);
+        i++;
         $("#Antwortbox").append(div);
-        div.click(clickhandler);
+        $(":button[id="+ui.draggable.attr('id') +"]").click(clear);
+        $(".hide[id=" +ui.draggable.attr('id')+"]").toggle();
+        console.log($(".inline.ui-draggable[id="+ui.draggable.attr('id') +"]"));
+        //$("div[class=inline],[id="+ui.draggable.attr('id') +"]").each(function(index){
+          //  $(index).draggable.css('color','red');
+           // $(index).draggable.draggable('disable');
+        //});
         ui.draggable.css('color','red');
         ui.draggable.draggable('disable');
     };
+
     function myHelper( event ) {
         return jQuery('<div/>', {
             class: 'draggie',
@@ -35,9 +43,15 @@ function init() {
             text: $(this).context.innerHTML
         });
     };
-    function clickhandler() {
-        $("div[class=inline.ui-draggable][id="+this.id+"]").draggable("enable");
-        $("div[class=inline.ui-draggable][id="+this.id+"]").css('color','black');
-        $(this).remove();
+
+    function clear(){
+        console.log("es passiert");
+        $(".inline.ui-draggable[id="+this.id+"]").draggable("enable").css('color','black');
+        $(".token[id="+this.id+"]").remove();
+    }
+
+    function enlarge() {
+            $(".hide[id=" +$(this).attr('id')+"]").toggle();
     };
+
 };
