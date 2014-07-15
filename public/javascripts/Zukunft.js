bfe89075e97504b10698c0aeb9cce6589cb7fe34
Robert S <robert.studtrucker@gmail.com>
2014-07-15 10:53:04 +0200

    Stylesheets für jeden Bereich eingeführt

--- public/javascripts/Zukunft.js
+++ public/javascripts/Zukunft.js
@@ -23,19 +23,21 @@ function init() {
         var textinhalt = ui.draggable.context.textContent;
         textinhalt = textinhalt.replace(',','');
         textinhalt = textinhalt.replace('.','');
-
+        textinhalt = textinhalt.replace('"','');
         var div = jQuery('<div/>', {
             class: 'token',
             id: ui.draggable.attr('id'),
-            html: "<p>"+textinhalt+"</p><div id='"+ui.draggable.attr('id') +"' class='hide'><p>"+testtext+"</p><a target='_blank' href='http://www.duden.de/suchen/dudenonline/"+ui.draggable.context.textContent+"'>INFO-Link</a><p><button id='"+ui.draggable.attr('id')+"'>löschen</button></p></div>"
-            });
+            html: "<p>"+textinhalt+"</p><div id='"+ui.draggable.attr('id') +"' class='hide'><p>"+testtext+"</p><a target='_blank' href='http://www.duden.de/suchen/dudenonline/"+ui.draggable.context.textContent+"'>INFO-Link</a></div>"
+        });
         div.click(enlarge);
         if($.inArray(textinhalt, testarray)> -1){
             $(div).data('korrekt','true');
             $(div).css('background','rgba(2, 255, 85, 0.16)');
         }else {$(div).data('korrekt','false');$(div).css('background','rgba(240, 128, 128, 0.44)');$(div).css('color','black');}
+        $("#Antwortbox").bind("contextmenu",function(e){return false;});
         $("#Antwortbox").append(div);
-        $(":button[id="+ui.draggable.attr('id') +"]").click(clear);
+        div.mousedown(clear);
+        //$(":button[id="+ui.draggable.attr('id') +"]").click(clear);
         $(".hide[id=" +ui.draggable.attr('id')+"]").toggle();
         $(".inline.ui-draggable[id="+ui.draggable.attr('id')+"]").draggable("disable").css('color','red');
     };
@@ -51,10 +53,11 @@ function init() {
         });
     };
 
-    function clear(){
-        console.log("es passiert");
-        $(".inline.ui-draggable[id="+this.id+"]").draggable("enable").css('color','black');
-        $(".token[id="+this.id+"]").remove();
+    function clear(ev) {
+        if (ev.which == 3) {
+        $(".inline.ui-draggable[id=" + this.id + "]").draggable("enable").css('color', 'black');
+        $(".token[id=" + this.id + "]").remove();
+        }
     }
 
     function enlarge() {
