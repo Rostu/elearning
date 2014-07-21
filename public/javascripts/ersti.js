1436b6e8936ba83e75c2e21859c67f2b26c2be70
Atrastea <mirko_daumann@web.de>
2014-07-21 15:27:00 +0200

    diverse veränderung im bereich ersti. (css,js sowie die page files)

--- public/javascripts/ersti.js
+++ public/javascripts/ersti.js
@@ -1,6 +1,86 @@
-/**
- * Created by David on 17.07.2014.
- */
+
+$( init );
+function init() {
+
+    var id=0;
+    var id_speicher = [];
+    var testarray = ["aufgeregt","Nervosität"];
+    var i = 0;
+    var infos = [];
+    var r_length=0;
+    var w_length = 0;
+    $(".inline").each(function() {
+        $(this).draggable({
+            containment: '#page',
+            revert: true,
+            helper: myHelper});
+    });
+
+    $('#Antwortbox').droppable( {
+        drop: handleDropEvent
+    });
+
+    function handleDropEvent( event, ui ) {
+        var textinhalt = ui.draggable.context.textContent;
+        var textinfos ="";
+        textinhalt = textinhalt.replace(',','');
+        textinhalt = textinhalt.replace('.','');
+        textinhalt = textinhalt.replace('"','');
+        for(i=0;i<infos.length;i++){
+            if(infos[i][0] === textinhalt)
+            {textinfos = infos[i][1]};
+        }
+        var div = jQuery('<div/>', {
+            class: 'token',
+            id: ui.draggable.attr('id'),
+            html: "<p>"+textinhalt+"</p><div id='"+ui.draggable.attr('id') +"' class='hide'>"
+        });
+        div.click(enlarge);
+        if($.inArray(textinhalt, testarray)> -1){
+            $(div).data('korrekt',true);
+            $(div).css('background','rgba(2, 255, 85, 0.16)');
+            r_length++;
+            update_balken();
+        }else {$(div).data('korrekt',false);$(div).css('background','rgba(240, 128, 128, 0.44)');$(div).css('color','black');w_length++;update_balken();}
+        $("#Antwortbox").bind("contextmenu",function(e){return false;});
+        $("#Antwortbox").append(div);
+        div.mousedown(clear);
+        //$(":button[id="+ui.draggable.attr('id') +"]").click(clear);
+        $(".hide[id=" +ui.draggable.attr('id')+"]").toggle();
+        $(".inline.ui-draggable[id="+ui.draggable.attr('id')+"]").draggable("disable").css('color','red');
+    };
+
+    function myHelper( event ) {
+        var textinhalt = $(this).context.innerHTML;
+        textinhalt = textinhalt.replace(',','');
+        textinhalt = textinhalt.replace('.','');
+        return jQuery('<div/>', {
+            class: 'draggie',
+            id: $(this).attr('id'),
+            text: textinhalt
+        });
+    };
+
+    function update_balken(){
+        //console.log($('#balken_innen1'));
+        $('#balken_innen1').css('width',r_length*8);
+        $('#balken_innen2').css('width',w_length*4);
+    }
+    function clear(ev) {
+        if (ev.which == 3) {
+            $(".inline.ui-draggable[id=" + this.id + "]").draggable("enable").css('color', 'black');
+            if($(this).data('korrekt')==true){
+                r_length--;
+            }else{w_length--;}
+            update_balken();
+            $(".token[id=" + this.id + "]").remove();
+        }
+    }
+
+    function enlarge() {
+        $(".hide[id=" +$(this).attr('id')+"]").toggle();
+    };
+};
 
 $(document).ready(function() {
     // TASK 2
@@ -24,6 +104,7 @@ $(document).ready(function() {
     slots.on("blur", function() {
         validate(this);
     });
+
 
     // TASK 4
     var a = ['planlos','auf Anhieb','strömen','reibungslos verläuft','flach fällt',
