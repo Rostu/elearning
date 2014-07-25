b764ecf55fefe4009e19c3746e79ee5b850acf00
daalft <blueygh2@gmail.com>
2014-07-25 12:03:15 +0200

    Update ersti JS.

--- public/javascripts/ersti3.js
+++ public/javascripts/ersti3.js
@@ -4,47 +4,51 @@
 // TASK 3
 
 $(document).ready(function() {
-    var t3w1 = ["anregend",  "ansprechend",  "einnehmend",  "einträglich",  "entwaffnend",  "erstaunlich",  "ertragreich",  "gewinnbringend",  "informativ",  "lohnend",  "reizvoll",  "unterhaltsam",  "vielsagend",  "wissenswert"];
-    var t3w2 = ["alltäglich",  "belanglos",  "einfallslos", "einschläfernd",  "eintönig",  "ermüdend",  "fade",  "langweilig", "monoton",  "öde",  "phantasielos",  "reizlos",  "trist",  "unbedeutend",  "unwichtig"];
-    var cont = $('.worddrag');
-    var t3w = [];
+    var whereami = document.location.href;
+    if (/.+?task3/.test(whereami)) {
+        var t3w1 = ["anregend",  "ansprechend",  "einnehmend",  "einträglich",  "entwaffnend",  "erstaunlich",  "ertragreich",  "gewinnbringend",  "informativ",  "lohnend",  "reizvoll",  "unterhaltsam",  "vielsagend",  "wissenswert"];
+        var t3w2 = ["alltäglich",  "belanglos",  "einfallslos", "einschläfernd",  "eintönig",  "ermüdend",  "fade",  "langweilig", "monoton",  "öde",  "phantasielos",  "reizlos",  "trist",  "unbedeutend",  "unwichtig"];
+        var cont = $('.worddrag');
+        var t3w = [];
 
-    for (var i = 0; i < t3w1.length; i++) {
-        t3w.push(makeDiv(t3w1[i], i));
-    }
-    for (i = 0; i < t3w2.length; i++) {
-        t3w.push(makeDiv(t3w2[i], i));
-    }
-    //t3w = shuffle(t3w);
-
-    for (i = 0; i < t3w.length; i++) {
-        cont.append(t3w[i]);
-    }
-
-    $('.word').draggable({revert: "invalid"});
-
-    $('.target').droppable({drop: function(e,ui) {
-        $(ui.draggable).attr("style","").appendTo($(this));
-
-    }});
-
-    function makeDiv(a, i) {
-        return "<div class='word' id='w"+ (i+1) +"'>" + a + "</div>";
-    }
-
-    function shuffle(array) {
-        var counter = array.length, temp, index;
-        while (counter > 0) {
-            index = Math.floor(Math.random() * counter);
-            counter--;
-            temp = array[counter];
-            array[counter] = array[index];
-            array[index] = temp;
+        for (var i = 0; i < t3w1.length; i++) {
+            t3w.push(makeDiv(t3w1[i], i));
         }
-        return array;
+        for (i = 0; i < t3w2.length; i++) {
+            t3w.push(makeDiv(t3w2[i], i));
+        }
+        //t3w = shuffle(t3w);
+
+        for (i = 0; i < t3w.length; i++) {
+            cont.append(t3w[i]);
+        }
+
+        $('.word').draggable({revert: "invalid"});
+
+        $('.target').droppable({drop: function(e,ui) {
+            $(ui.draggable).attr("style","").appendTo($(this));
+
+        }});
+
+        function makeDiv(a, i) {
+            return "<div class='word' id='w"+ (i+1) +"'>" + a + "</div>";
+        }
+
+        function shuffle(array) {
+            var counter = array.length, temp, index;
+            while (counter > 0) {
+                index = Math.floor(Math.random() * counter);
+                counter--;
+                temp = array[counter];
+                array[counter] = array[index];
+                array[index] = temp;
+            }
+            return array;
+        }
+    } else
+    {
+
     }
-
-
 });
 
 function validate () {
