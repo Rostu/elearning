fbc7c0ef4c7097e1925b88a944659f981769565b
Robert Studtrucker <robert.studtrucker@gmail.com>
2014-08-01 12:03:56 +0200

    China

--- public/javascripts/china.js
+++ public/javascripts/china.js
@@ -1,36 +1,81 @@
-
 $( init );
 function init() {
-
-    $(".inline").each(function() {
-        $(this).draggable({
-            containment: '#page',
-            revert: true,
-            helper: myHelper});
+    var evaluation = [ { "id": 0, "class": 3, "text": "Austauschstudent" },
+                       { "id": 1, "class": 1, "text": "BWL" },
+                       { "id": 2, "class": 3, "text": "Student" },
+                       { "id": 3, "class": 1, "text": "Banking and Finance" },
+                       { "id": 4, "class": 2, "text": "Fachhochschule" },
+                       { "id": 5, "class": 2, "text": "Summer School" },
+                       { "id": 6, "class": 4, "text": "Partnerhochschule" },
+                       { "id": 7, "class": 2, "text": "Universität" },
+                       { "id": 8, "class": 3, "text": "Absolvent" },
+                       { "id": 9, "class": 1, "text": "Angewandte Wirtschaftssprachen" },
+                       { "id": 10, "class": 4, "text": "Frontalunterricht" },
+                       { "id": 11, "class": 4, "text": "Auswendiglernen" },
+                       { "id": 12, "class": 4, "text": "Vorlesungsinhalte" },
+                       { "id": 13, "class": 3, "text": "Professor" },
+                       { "id": 14, "class": 4, "text": "Diskussionen" },
+                       { "id": 15, "class": 3, "text": "Lehrperson" },
+                       { "id": 16, "class": 4, "text": "Lektüre" },
+                       { "id": 17, "class": 2, "text": "TU München" },
+                       { "id": 18, "class": 3, "text": "Studierende" } ];
+    
+    var applyToAnswers = function( fn ) {
+        // Takes a unary function and assings is to every anwser box
+        var answerArray = [ "#Antwortbox1", "#Antwortbox2", "#Antwortbox3", "#Antwortbox4" ];
+        
+        $.map( answerArray, fn);
+    };
+    
+    // assign drop behaviour
+    applyToAnswers( function( id ) {
+        $( id ).droppable( {
+            drop: handleDropEvent
+        })
     });
-
-    $('#Antwortbox').droppable( {
-        drop: handleDropEvent
-    });
-
+    
     function handleDropEvent( event, ui ) {
         var textinhalt = ui.draggable.context.textContent;
-        textinhalt = textinhalt.replace(',','');
-        textinhalt = textinhalt.replace('.','');
+
         var div = jQuery('<div/>', {
             class: 'token',
             id: ui.draggable.attr('id'),
             text: textinhalt
         });
+
+        textinhalt = textinhalt.replace(',','');
+        textinhalt = textinhalt.replace('.','');
+        
         div.click(clear);
-        $("#Antwortbox").append(div);
+
+        // get drop id and append it to where it was dropped
+        var draggedDivId  = $(this).closest('div').attr('id');
+        var answerDragged = parseInt( draggedDivId.slice(-1) );
+
+        var realAnswer    = ($.grep( evaluation, function( n ) {
+            return n['id'] == parseInt( ui.draggable.attr('id') )
+        }))[0]["class"];
+
+        // attach the elements
+        $( "#" + draggedDivId ).append( div ).append( "<br />" );        
         $(".inline.ui-draggable[id="+ui.draggable.attr('id')+"]").draggable("disable").css('color','grey');
+
+        // is right?
+        var rightAnswered;
+        
+        if (answerDragged = realAnswer) {
+            rightAnswered = true;
+        } else {
+            rightAnswered = false;
+        }
     };
 
     function myHelper( event ) {
         var textinhalt = $(this).context.innerHTML;
+
         textinhalt = textinhalt.replace(',','');
         textinhalt = textinhalt.replace('.','');
+
         return jQuery('<div/>', {
             class: 'draggie',
             id: $(this).attr('id'),
@@ -43,5 +88,11 @@ function init() {
         $(this).remove();
     }
 
-
+    
+    $(".inline").each(function() {
+        $(this).draggable({
+            containment: '#page',
+            revert: true,
+            helper: myHelper});
+    });
 };
