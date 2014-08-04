f61642b4737d148ed0def66cf521733f838b044a
daalft <blueygh2@gmail.com>
2014-08-04 11:23:49 +0200

    Created generationen task 2 JS.

--- public/javascripts/generationen_task2.js
+++ public/javascripts/generationen_task2.js
@@ -1,1 +1,124 @@
+/**
+ * Created by David on 03.08.2014.
+ */
+$(document).ready(function() {
+    var count = 0;
+    var a = ["Friedhofs-",
+        "Erwachsen-",
+        "Auseinander-",
+        "Schul-",
+        "Leistungs-",
+        "Kontakt-",
+        "Flucht-",
+        "Steh-",
+        "Party-"];
+    var b = ["-gemüse",
+        "-werden",
+        "-klaffen",
+        "-zeit",
+        "-bereitschaft",
+        "-freude",
+        "-röhre",
+        "-vermögen",
+        "-fummel"];
+    var c = [];
+    for (var i = 0; i < a.length; i++) {
+        c.push(makeTok(a[i], i));
+    }
+    c = shuffle(c);
+    for (i = 0; i < c.length; i++) {
+        $('#ltext').append(c[i]);
+    }
+    c = [];
+    for (i = 0; i < b.length; i++) {
+        c.push(makeTok(b[i],i));
+    }
+    c = shuffle(c);
+    for (i = 0; i < c.length; i++) {
+        $('#rtext').append(c[i]);
+    }
+    var pd,pp;
+    $('.tok').draggable({revert:'invalid'});
+    $('.drop2').droppable({drop: function(e,ui) {
+        var drag = $(ui.draggable).attr("style","").attr("style","position:relative");
 
+        var parent = drag.parent();
+        drag.detach();
+        var num = this.id.substring(1);
+        var other = "a" + (num==2?"1":"2");
+        if (parent[0].id.substring(0,1)==="l") {
+            if (num == 2) {
+                flash(this);
+                parent.append(drag);
+                return;
+            }
+        }
+        if (parent[0].id.substring(0,1)==="r") {
+            if(num == 1) {
+                flash(this);
+                parent.append(drag);
+                return;
+            }
+        }
+        $(this).text(drag.text());
+        var o = $('#'+other);
+        if (o.text()) {
+            if (pd.attr("id").substring(1) == drag.attr("id").substring(1)) {
+                flash(null, '#90EE90');
+                $('#message').show().effect("puff", 2000, function() {
+                    $('#a1').text("");
+                    $('#a2').text("");
+                    count++;
+                    if (count > 9) {
+                        // end task
+                    }
+                });
+            } else {
+                flash();
+                $('.drop2').css({'color':'darkred'});
+                $('#message2').show().effect("puff", 2000, function() {
+                    pp.append(pd);
+                    parent.append(drag);
+                    $('#a1').text("");
+                    $('#a2').text("");
+                    $('.drop2').css({'color':'green'});
+                });
+            }
+        } else {
+            pd = drag;
+            pp = parent;
+        }
+    }});
+    function shuffle(array) {
+        var counter = array.length, temp, index;
+        // While there are elements in the array
+        while (counter > 0) {
+            // Pick a random index
+            index = Math.floor(Math.random() * counter);
+            // Decrease counter by 1
+            counter--;
+            // And swap the last element with it
+            temp = array[counter];
+            array[counter] = array[index];
+            array[index] = temp;
+        }
+        return array;
+    }
+
+    function makeTok(a, i) {
+        return "<div class='tok' id='t" + (i+1) + "'>" + a + "</div>";
+    }
+
+    function flash(elem, col) {
+        col = col || '#F08080';
+        if (elem) {
+            $(elem).animate({'background-color':col, 'opacity':0.2}, 1000, "easeInOutQuart", function() {
+                $(this).css({'background-color': 'white', 'opacity': 1.0});
+            });
+        } else {
+            $('.drop2').animate({'background-color':col, 'opacity':0.2}, 1000, "easeInOutQuart", function() {
+                $(this).css({'background-color': 'white', 'opacity':1.0});
+            });
+        }
+    }
+});
