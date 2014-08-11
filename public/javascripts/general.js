a2b993e7ecabbb48d118355336a2f61399165553
daalft <blueygh2@gmail.com>
2014-08-11 18:16:44 +0200

    Added shuffle method to general JS.

--- public/javascripts/general.js
+++ public/javascripts/general.js
@@ -44,3 +44,15 @@ $(document).ready(function() {
         $('.helpmenu').detach();
     }
 });
+
+function shuffle(array) {
+    var counter = array.length, temp, index;
+    while (counter > 0) {
+        index = Math.floor(Math.random() * counter);
+        counter--;
+        temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+    return array;
+}
