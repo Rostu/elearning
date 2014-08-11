f170271512e350ebb79c48b9f70c4d2b59f6ffa6
daalft <blueygh2@gmail.com>
2014-08-11 17:35:27 +0200

    Added arrays.

--- public/javascripts/generationen_task4.js
+++ public/javascripts/generationen_task4.js
@@ -2,6 +2,30 @@
  * Created by David on 03.08.2014.
  */
 $(document).ready(function() {
+
+    var testarray = ["etw. (auseinander) schneiden", "sich annähern", "Ausruhen", "Anziehen", "Sich verbiegen", "Schießen", "PC in die Garage auslagern", "Abstellplatz für PC", "konstanter Reichtum (Besitz)", "aufrechter Stand", "reinigen", "streicheln"];
+    var array = ["Abstand vergrößern","Anstrengen","Gehen","etw./jmd. (ab-)setzen [um Ruhe zu haben]","Ausdauer","streiten"];
+    var infos =["Auseinanderschneiden bedeutet etwas mit einer Schere oder ähnlichem im konkreten Wortsinn zu trennen; „Lebenswelten“ ist ein abstrakter Begriff, also nichts, was sich auseinanderschneiden lässt.",
+        "auseinanderklaffen lässt sich in auseinander + klaffen zerteilen. „auseinander“ bedeutet sich entfernen von jemandem oder etwas, also das Gegenteil von „sich annähern“",
+        "Wenn ich mich nicht ins Zeug lege, dann ruhe ich mich aus. Ausruhen ist das Gegenteil von ins Zeug legen.",
+        "hier wurdest du aufs Glatteis geführt: Anziehen hat nichts mit „ins Zeug legen“ zu tun. Man kann beispielsweise Kleidung anziehen.",
+        "Das bedeutet sich verformen oder krümmen, bzw. verändern.",
+        "Schießen ist ein Verb mit der Bedeutung jmd. töten, feuern, knallen und passt somit von der Wortbedeutung nicht zum Satz.",
+        "Wenn man etwas (PC) von einer Stelle weg an eine andere verlagert (Garage). Hier ist nicht das „verlagern“ gemeint sondern das „zur Ruhe kommen“.",
+        "einen Abstellplatz für den PC bezeichnet das Gegenteil von  „Jugendlichen vor dem PC platzieren.“",
+        "„konstant“ würde zwar zu Stehvermögen passen. Dennoch ist dieses Wort in Verbindung mit „Reichtum“ darauf bezogen, dass man dauerhaft Vermögenswerte oder Besitz ansammelt. Reichtum ist kein Mittel zur Erlernung von Disziplin und Erfolg.",
+        "aufrechtes Stehen bezieht sich eher auf den physischen Aspekt und ist ebenso kein Mittel zur Erlernung von Disziplin und Erfolg.",
+        "„reinigen“ bedeutet etwas säubern bzw. Schmutz oder Flecken von etwas entfernen.",
+        "„streicheln“ bezeichnet ein Verb „mit leichten, gleitenden Bewegungen der Hand sanft, liebkosend berühren“"
+    ];
+    $('.mco').toArray().map(function(e) {
+        var b = [];
+        b.push(array.shift());
+        b.push(testarray.shift());
+        b.push(testarray.shift());
+        b = shuffle(b);
+
+    });
     $('#check').click(function() {
         $('.mco').toArray().map(function(e) {
             $(e).children('input').toArray().map(function(f) {
