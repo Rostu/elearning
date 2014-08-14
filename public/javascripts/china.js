955450f4f83c359a0033bb15aa8711ccb7725cd3
Andre P. Beyer <beyeran@gmail.com>
2014-08-14 23:37:57 +0200

    Lib created and task 2 finised

--- public/javascripts/china.js
+++ public/javascripts/china.js
@@ -1,98 +1,68 @@
-$( init );
-function init() {
-    var evaluation = [ { "id": 0, "class": 3, "text": "Austauschstudent" },
-                       { "id": 1, "class": 1, "text": "BWL" },
-                       { "id": 2, "class": 3, "text": "Student" },
-                       { "id": 3, "class": 1, "text": "Banking and Finance" },
-                       { "id": 4, "class": 2, "text": "Fachhochschule" },
-                       { "id": 5, "class": 2, "text": "Summer School" },
-                       { "id": 6, "class": 4, "text": "Partnerhochschule" },
-                       { "id": 7, "class": 2, "text": "Universität" },
-                       { "id": 8, "class": 3, "text": "Absolvent" },
-                       { "id": 9, "class": 1, "text": "Angewandte Wirtschaftssprachen" },
-                       { "id": 10, "class": 4, "text": "Frontalunterricht" },
-                       { "id": 11, "class": 4, "text": "Auswendiglernen" },
-                       { "id": 12, "class": 4, "text": "Vorlesungsinhalte" },
-                       { "id": 13, "class": 3, "text": "Professor" },
-                       { "id": 14, "class": 4, "text": "Diskussionen" },
-                       { "id": 15, "class": 3, "text": "Lehrperson" },
-                       { "id": 16, "class": 4, "text": "Lektüre" },
-                       { "id": 17, "class": 2, "text": "TU München" },
-                       { "id": 18, "class": 3, "text": "Studierende" } ];
+/*
+  The only Thing which had to be adjusted for all the china programs was
+  the "handleDropEvent" function.
+
+  The remaining functionality is obtained via the "china_lib.js".
+*/
+
+function handleDropEvent( event, ui ) {
+    // drop event
+
+    // These function is used to obtain the words for the lecture. Since
+    // JQuery's mysterious scope, the drop event must be passed as well.
+    var textinhalt = ui.draggable.context.textContent;
     
-    var applyToAnswers = function( fn ) {
-        // Takes a unary function and assings is to every anwser box
-        var answerArray = [ "#Antwortbox1", "#Antwortbox2", "#Antwortbox3", "#Antwortbox4" ];
+    var div = jQuery('<div/>', {
+        class: 'token',
+        id:     ui.draggable.attr('id'),
+        text: textinhalt
+    });
         
-        $.map( answerArray, fn);
-    };
+    textinhalt = textinhalt.replace(',','');
+    textinhalt = textinhalt.replace('.','');
+        
+    div.click(clear);
+
+    // get drop id and append it to where it was dropped
+    var draggedDivId = $(this).closest('div').attr('id');
+    var answerDragged = parseInt( draggedDivId.slice(-1) );
+    var realAnswer    = ui.draggable.context.firstChild.id;
+
+    // is right?
+    var correct_p;
+        
+    if (answerDragged == realAnswer) {
+        correct_p = true;
+    } else {
+        correct_p = false;
+    }
     
-    // assign drop behaviour
+    // attach the elements
+    if (correct_p == true)
+    {
+        $( "#Antwortbox" + answerDragged ).append( div.animate({backgroundColor: "#0f0"}, 2000) ).append( "<br />" );
+    }
+    else if (correct_p == false)
+    {
+        $( "#Antwortbox" + answerDragged ).append( div.animate({backgroundColor: "#f00"}, 2000) ).append( "<br />" );
+    }
+    
+    $(".inline.ui-draggable[id="+ui.draggable.attr('id')+"]").draggable("disable").css('color','grey');
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// main function
+////////////////////////////////////////////////////////////////////////////////
+function init() {
+    loadWords( "/javascripts/china_correct_words.json", "correct", true );
+
+    // add droppfields
     applyToAnswers( function( id ) {
         $( id ).droppable( {
             drop: handleDropEvent
-        })
-    });
-    
-    function handleDropEvent( event, ui ) {
-        var textinhalt = ui.draggable.context.textContent;
-
-        var div = jQuery('<div/>', {
-            class: 'token',
-            id: ui.draggable.attr('id'),
-            text: textinhalt
         });
-
-        textinhalt = textinhalt.replace(',','');
-        textinhalt = textinhalt.replace('.','');
-        
-        div.click(clear);
-
-        // get drop id and append it to where it was dropped
-        var draggedDivId  = $(this).closest('div').attr('id');
-        var answerDragged = parseInt( draggedDivId.slice(-1) );
-
-        var realAnswer    = ($.grep( evaluation, function( n ) {
-            return n['id'] == parseInt( ui.draggable.attr('id') )
-        }))[0]["class"];
-
-        // attach the elements
-        $( "#" + draggedDivId ).append( div ).append( "<br />" );        
-        $(".inline.ui-draggable[id="+ui.draggable.attr('id')+"]").draggable("disable").css('color','grey');
-
-        // is right?
-        var rightAnswered;
-        
-        if (answerDragged = realAnswer) {
-            rightAnswered = true;
-        } else {
-            rightAnswered = false;
-        }
-    };
-
-    function myHelper( event ) {
-        var textinhalt = $(this).context.innerHTML;
-
-        textinhalt = textinhalt.replace(',','');
-        textinhalt = textinhalt.replace('.','');
-
-        return jQuery('<div/>', {
-            class: 'draggie',
-            id: $(this).attr('id'),
-            text: textinhalt
-        });
-    };
-
-    function clear(){
-        $(".inline.ui-draggable[id="+this.id+"]").draggable("enable").css('color','black');
-        $(this).remove();
-    }
-
-    
-    $(".inline").each(function() {
-        $(this).draggable({
-            containment: '#page',
-            revert: true,
-            helper: myHelper});
     });
+
 };
+
+$( init );
--- public/javascripts/china1.js
+++ public/javascripts/china1.js
@@ -1,76 +1,9 @@
-////////////////////////////////////////////////////////////////////////////////
-// loading and applying words
-////////////////////////////////////////////////////////////////////////////////
-var applyWords = function( wordArray, group ) {
-    // Takes an array of hashes, containing an id, a text and a boolean variable "fits"
-    // and makes all words in that array draggable in the DOCUMENT
-    // for each textbox
-    $.each( $( "#Textbox p" ), function( x ) {
-        var original_text = $(this).html()
-        // for each keyword
-        $.map( wordArray, function( n ) {
-            var regex = new RegExp( n['text'], "g");
-            var replaceString = "<div id=\"" + n.id + "\" class=\"inline ui-draggable\""
-                + " style=\"color: #333333\"><div id=\"" + group + "\">"
-                + n.text + "</div></div>";
-            // replace phrase if found
-            original_text = original_text.replace(regex, replaceString);
-        });
-        $(this).html( original_text );
-    });
-};
+/*
+  The only Thing which had to be adjusted for all the china programs was
+  the "handleDropEvent" function.
 
-var loadWords = function( jsonpath, group ) {
-    // Loads a JSON file from the given part and returns it's objects.
-    // These function is used to obtain the words for the lecture.
-    $.getJSON( jsonpath, function( json ) {
-        var wordArray = [];
-        
-        $.each( json.words, function( obj, content ) {            
-            wordArray.push( content );
-        });
-        
-        // strangely there seems no way to escape the
-        // "getJSON" scope. So "applyWords" must be
-        // called inside it.
-        
-        applyWords( wordArray, group );
-
-        // make the words draggable:
-        // I'd prefer this outside, but it would be out
-        // of scope... again!
-        $(".inline").each(function() {
-            $(this).draggable(
-                {
-                    containment: '#page',
-                    revert: true,
-                    helper: myHelper
-                }
-            );
-        });
-    })
-};
-
-////////////////////////////////////////////////////////////////////////////////
-// handle drag & drop events
-////////////////////////////////////////////////////////////////////////////////
-var clear = function () {
-    $(".inline.ui-draggable[id="+this.id+"]").draggable("enable").css('color','black');
-    $(this).remove();
-}
-
-var myHelper = function ( event ) {
-    var textinhalt = $(this).context.textContent;
-
-    textinhalt = textinhalt.replace(',','');
-    textinhalt = textinhalt.replace('.','');
-
-    return jQuery('<div/>', {
-        class: 'draggie',
-        id: $(this).attr('id'),
-        text: textinhalt
-    });
-};
+  The remaining functionality is obtained via the "china_lib.js".
+*/
 
 var handleDropEvent = function ( event, ui ) {
     // Handles the functionallity of dropping a word into the box and
@@ -118,9 +51,16 @@ var handleDropEvent = function ( event, ui ) {
 ////////////////////////////////////////////////////////////////////////////////
 var init = function () {
     // apply correct words to text
-    loadWords( "/javascripts/china_correct_words.json", "correct" );
-    loadWords( "/javascripts/china_wrong_words.json", "wrong" );
+    loadWords( "/javascripts/china_correct_words.json", "correct", false );
+    loadWords( "/javascripts/china_wrong_words.json", "wrong", false );
 
+    // assign drop behaviour
+    applyToAnswers( function( id ) {
+        $( id ).droppable( {
+            drop: handleDropEvent
+        })
+    });
+    
     // make a dropfield
     $('#Antwortbox').droppable( {
         drop: handleDropEvent
