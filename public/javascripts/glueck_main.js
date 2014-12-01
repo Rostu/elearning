382455ba9e3b1d241423c4771606ba03366db36a
Atrastea <mirko_daumann@web.de>
2014-12-02 00:23:47 +0100

    Fixed Page Errors, Added icons for start pages

--- public/javascripts/glueck_main.js
+++ public/javascripts/glueck_main.js
@@ -4,15 +4,15 @@
 
 $(document).ready(function() {
    $('#l1').click(function() {
-        document.location.href = "glueck_gedicht";
+        document.location.href = "glueck_task1a";
    });
     $('#l2').click(function() {
-        document.location.href = "glueck_interview";
+        document.location.href = "glueck_task2";
     });
     $('#l3').click(function() {
-        document.location.href = "glueck_redensarten";
+        document.location.href = "glueck_task3";
     });
     $('#l4').click(function() {
-        document.location.href = "glueck_kwr";
+        document.location.href = "glueck_task4";
     });
 });
