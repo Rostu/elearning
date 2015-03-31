b094ed359be5bc133f16f1fda254a3a8a2dd991e
Robert S <robert.studtrucker@gmail.com>
2015-03-31 11:38:28 +0200

    veggieday1 links added veggieday 2+3 small design changes

--- public/javascripts/veggieday_Textverstehen_Schluesselwoerter_in_Boulevardzeitungen.js
+++ public/javascripts/veggieday_Textverstehen_Schluesselwoerter_in_Boulevardzeitungen.js
@@ -1,11 +1,10 @@
 $(init);
 function init() {
+
     //shows the info buttons on the left side
-    $('.redstripe').show();
-    //add a content div to the second info button; first one is reserved
-    jQuery('<div/>', {
-        class: 'redtext'
-    }).appendTo('#info2');
-    $(".redtext").append(" <div> <a id='infolink' class='redlink' target='blank' href=\"http://www.duden.de/suchen/dudenonline\">Duden </a> </div>");
+    $('#info1').show();
+    $('#info2').show();
+    $('#info1').html("<a id='infolink' class='redlink' target='_tab' href=\"http://www.duden.de/suchen/dudenonline\">Duden </a> ");
+    $('#info2').html("<a id='infolink' class='redlink' target='_tab' href=\'http://wortschatz.uni-leipzig.de/'>Wortschatzportal</a> ");
 
 };
