b1b06de3ef3ae24cd42aeaefab6850229182e91e
Robert S <robert.studtrucker@gmail.com>
2015-05-28 18:46:27 +0200

    added glueck_video_kleeblatt

---
+++ public/javascripts/glueck_video_kleeblatt.js
@@ -0,0 +1,30 @@
+$( init );
+function init() {
+    $('#info3').show();
+
+
+    //since svg Dom is a bit trixky to handle with jquery we have to pre select some element with plain javaScript
+    window.onload=function() {
+        // Get the Object by ID
+        var a = document.getElementById("klee-svg");
+        // Get the SVG document inside the Object tag
+        var svgDoc = a.contentDocument;
+        // Get one of the SVG items by ID;
+        var svgItem = svgDoc.getElementById("Freund");
+        var allItems = svgDoc.getElementsByClassName("form");
+        //set the fill color of all svg elements and add click-event listener
+        for (i = 0; i < allItems.length; ++i) {
+            allItems[i].setAttribute("fill", "lightgrey");
+            $(allItems[i]).on("click", leafClick);
+        }
+
+    };
+
+    function leafClick(){
+        this.setAttribute("fill", "green");
+    }
+
+
+
+
+}
