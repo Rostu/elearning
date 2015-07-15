bd19275c663f513e4a69e5f35527cb002715c8d8
Robert S <robert.studtrucker@gmail.com>
2015-07-15 15:47:48 +0200

    changed feedback

--- routes/feedback.js
+++ routes/feedback.js
@@ -11,15 +11,16 @@ exports.new_feedback = function(req, res){
     var seite = req.body.seite;
     var design = req.body.design;
     var aufgabe = req.body.aufgabe;
-    var funktion = req.body.funktion;
+    var zusatz = req.body.zusatz;
+    var feedback = req.body.feedback;
+    var anmerkung = req.body.anmerkung;
+
+
     //even if classic codeinjection is not possible like in SQL-Databases we are cutting some chars just because :-)
     //for mongodb important to check if design, aufgabe and funtion are strings to prevent injection of json objects
-    if ((typeof design === "string") && (typeof aufgabe === "string")&&(typeof funktion === "string"))    {
-
-        design = design.replace(/[-+;:_\(\)!"§$%&\[\]\{\}#*']/g, '');
-        aufgabe = aufgabe.replace(/[-+;:_\(\)!"§$%&\[\]\{\}#*']/g, '');
-        funktion = funktion.replace(/[-+;:_\(\)!"§$%&\[\]\{\}#*']/g, '');
-        dbh.save_feedback(seite,design,aufgabe,funktion);
+    if (typeof anmerkung === "string") {
+        anmerkung = anmerkung.replace(/[-+;:_\(\)!"§$%&\[\]\{\}#*']/g, '');
+        dbh.save_feedback(seite,design,aufgabe,zusatz,feedback,anmerkung);
     }
 
     res.render(seite);
