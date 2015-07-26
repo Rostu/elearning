4d112d509ed06d0fbc1dfba2d56b9127022ea41a
cmich <christopher@cmich.eu>
2015-07-26 21:09:43 +0200

    first clickable prototype

--- routes/stanford.js
+++ routes/stanford.js
@@ -1,14 +1,16 @@
 var NLP = require('stanford-corenlp');
 
-var coreNLP = new NLP.StanfordNLP({"nlpPath": __dirname + "/corenlp","version":"3.5.2"}, function(err) {
-    coreNLP.process('Glück ist, wenn die Sonne scheint.', function(err, result) {
-        console.log(err,JSON.stringify(result));
-        console.log('out')
+exports.get_request = function(req, res) {
+
+    var coreNLP = new NLP.StanfordNLP({"nlpPath": __dirname + "/corenlp", "version": "3.5.2"}, function (err) {
+        var sentences = req.param("sentences");
+        coreNLP.process(sentences, function(err, result) {
+            //console.log(err,JSON.stringify(result));
+            if(err){
+                console.log(err);
+            }else{
+                res.send(result);
+            }
+        });
     });
-});
-
-exports.get_request = function(req, res){
-
-    console.log('test');
-
-};
+}
