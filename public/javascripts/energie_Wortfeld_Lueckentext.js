d849486e3091be11089a6a74981acbe8fa1744d9
Robert Studtrucker <robert.studtrucker@gmail.com>
2016-03-01 12:56:27 +0100

    energie_wortfeld_Lueckentext nearly finished

--- public/javascripts/energie_Wortfeld_Lueckentext.js
+++ public/javascripts/energie_Wortfeld_Lueckentext.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-    	$('#info1').show();
-    	$('#info1').append("<a id='infolink1' class='redlink' href='#'>Das ist ein Info-Link</a>");
+
 	var data = [
-		{token:"Fahrzeuge",tag:"noun",number:"PLU",gold:["Fahrzeuge","Wagen","Solarautos","Solarfahrzeuge"]},
-		{token:"Solarautos",tag: "noun",number:"PLU",gold:["Fahrzeuge","Autos","Wagen","Solarautos","Solarfahrzeuge"]},
-		{token:"flach",tag:"adjective",number:"null",gold:["flach","dünn","platt","schmal"]},
-		{token:"Elektromotoren",tag:"noun",number:"PLU",gold:["Elektromotoren","Motoren","Antriebe"]},
+		{token:"Autokunde",tag:"noun",number:"PLU",gold:["Autos","Solarautos","Solarfahrzeuge"]},
+		{token:"Autokunde",tag: "noun",number:"PLU",gold:["Fahrzeuge","Wagen","Solarautos","Solarfahrzeuge"]},
+		{token:"flach",tag:"adjective",number:"null",gold:["flach","dünn","schmal"]},
+		{token:"V-Motoren",tag:"noun",number:"PLU",gold:["Elektromotoren","Antriebe", "Elektroantriebe"]},
 		{token:"rasant",tag:"adjective",number:"null",gold:["schnell","zügig"]},
 		{token:"Batterie",tag:"noun",number:"SIN",gold:["Batterie","Energiespeicher","Speicher"]},
 		{token:"super",tag:"adjective",number:"null",gold:["prima","gut"]},
@@ -19,11 +18,16 @@ $(document).ready(function() {
 	//var lastUpdate = $.post('/echo/json/', {json:JSON.stringify({'lastUpdate':"Hello World"})});
 
 	var luecken = $('.luecke');
-
+	$("#wrapper").on( "click",".wrong", function(elem) {
+		$("#info3").show();
+		$("#info3").empty();
+		$("#info3").append("<p>"+$(this).attr("errMSG")+"</p>");
+	});
 	luecken.on("blur", function() {
 		var query = $(this).val();
 		var gold = data[parseInt($(this).attr("id"))];
 		var inputfield = $(this);
+		$(this).removeClass("wrong");
 		if(!$(this).hasClass("checking") && !$(this).hasClass("done") && query.length > 2){
 			$(this).addClass("checking");
 			if($.inArray(query, gold.gold) > -1 ){
@@ -31,11 +35,13 @@ $(document).ready(function() {
 				$(this).removeClass("wrong");
 				$(this).addClass("done");
 				$(this).prop('disabled', true);
+				if($("#info3").is(':visible')){$("#info3").toggle();}
 				console.log("in list, therefor correct");
 			}else if(Levenshtein_inArray(query, gold.gold)){
 				$(this).removeClass("checking");
 				$(this).removeClass("wrong");
 				$(this).addClass("done");
+				if($("#info3").is(':visible')){$("#info3").toggle();}
 				$(this).val(get_Levenshtein_correct(query, gold.gold));
 				error_message = "Da hattest du dich verschrieben, schau dir die richtige Schreibweise nochmal an!";
 				console.log(error_message);
@@ -50,39 +56,59 @@ $(document).ready(function() {
 						console.log(baseformResponse);
 						//this ajax call gets a corresponding entry from the morphology lexicon in the database
 						//the results can be used to check if the wordform and numerus match the gold standard
-						//by doing the request like that, we create a jquerry deferred object, this makes the code more readable
 						var DML_check = $.get('/dml_get', {query:query});
-						console.log(gold.tag);
+						console.log("Gold Wortart: "+gold.tag);
 						if(gold.tag === "noun"){
 							var domain = $.get('/wl_domain', {query:baseformResponse[0][0],limit:20});
 							$.when(DML_check, domain).done(function (dmlResponse,domainResponse ) {
 								if(domainResponse[0] != "") {
-									if (false){
+									console.log(domainResponse[0]);
+									if ($.inArray(gold.token, domainResponse[0]) > -1){
 										$(inputfield).removeClass("checking");
 										$(inputfield).removeClass("wrong");
 										$(inputfield).addClass("done");
+										if($("#info3").is(':visible')){$("#info3").toggle();}
+										if(dmlResponse[0] != ""){
+											var infos = dmlResponse[0].morphology;
+											var test = true;
+											console.log(infos);
+											$.each(infos, function (index, b) {
+												if (b.indexOf("number:"+gold.number) > -1) {
+													test = false;
+												}
+											});
+											if(test){
+												error_message = "Das ist ein passendes Wort, aber es sollte in der Mehrzahl stehen.";
+												$(inputfield).attr("errMSG",error_message);
+												$(inputfield).removeClass("done");
+												$(inputfield).addClass("wrong");
+											}
+										}
+
 									}else{
 										if(dmlResponse[0] != ""){
 											var infos = dmlResponse[0].morphology;
 											$.each(infos, function (a, b) {
-												if (b.indexOf("wordclass:"+gold.tag) > -1 && b.indexOf("number:"+gold.number)) {
+												var test = false;
+												if (b.indexOf("wordclass:"+gold.tag) > -1 && b.indexOf("number:"+gold.number) > -1) {
 													$(inputfield).removeClass("checking");
 													$(inputfield).addClass("wrong");
-													error_message = "Dieses Wort passt hier nicht hin";
-													console.log(error_message);
+													error_message = "Das ist die richtige Wortart, aber dieses Wort macht hier keinen Sinn.";
+													test = true;
 												}
 												if (b.indexOf("wordclass:"+gold.tag) === -1) {
 													$(inputfield).removeClass("checking");
 													$(inputfield).addClass("wrong");
-													error_message += "Das ist die Falsche Wortart, in diese Lücke gehört ein: "+gold.tag+"\n";
-													console.log(error_message);
+													error_message += "Das ist die falsche Wortart, in diese Lücke gehört ein Nomen";
+													test = true;
 												}
-												if (b.indexOf("wordclass:"+gold.tag) === -1) {
+												if (b.indexOf("number:"+gold.number) === -1) {
 													$(inputfield).removeClass("checking");
 													$(inputfield).addClass("wrong");
 													error_message += "Das ist der falsche Numerus";
-													console.log(error_message);
+													test = true;
 												}
+												if(test){$(inputfield).attr("errMSG",error_message);return false}
 											});
 										}
 									}
@@ -98,27 +124,26 @@ $(document).ready(function() {
 										$(inputfield).removeClass("checking");
 										$(inputfield).removeClass("wrong");
 										$(inputfield).addClass("done");
+										if($("#info3").is(':visible')){$("#info3").toggle();}
 									}else{
 										if(dmlResponse[0] != ""){
 											var infos = dmlResponse[0].morphology;
 											$.each(infos, function (a, b) {
-												if (b.indexOf("wordclass:"+gold.tag) > -1 && b.indexOf("number:"+gold.number)) {
+												if (b.indexOf("wordclass:"+gold.tag) > -1) {
 													$(inputfield).removeClass("checking");
 													$(inputfield).addClass("wrong");
 													error_message = "Dieses Wort passt hier nicht hin";
 													console.log(error_message);
+													$(inputfield).attr("errMSG",error_message);
+													return false;
 												}
 												if (b.indexOf("wordclass:"+gold.tag) === -1) {
 													$(inputfield).removeClass("checking");
 													$(inputfield).addClass("wrong");
-													error_message += "Das ist die Falsche Wortart, in diese Lücke gehört ein: "+gold.tag+"\n";
+													error_message += "Das ist die falsche Wortart, in diese Lücke gehört ein: Adjektiv";
 													console.log(error_message);
-												}
-												if (b.indexOf("wordclass:"+gold.tag) === -1) {
-													$(inputfield).removeClass("checking");
-													$(inputfield).addClass("wrong");
-													error_message += "Das ist der falsche Numerus";
-													console.log(error_message);
+													$(inputfield).attr("errMSG",error_message);
+													return false;
 												}
 											});
 										}
@@ -131,7 +156,7 @@ $(document).ready(function() {
 						$(inputfield).removeClass("checking");
 						$(inputfield).addClass("wrong");
 						error_message = "Dieses Wort kann ich leider nicht finden";
-						console.log(error_message);
+						$(inputfield).attr("errMSG",error_message);
 					}
 				});
 			}
